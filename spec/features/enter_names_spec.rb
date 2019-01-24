1f55a76c065669d84e4ed2277eac87efaf9c249e
James <james.rudrum@hyperisland.se>
2019-01-24 15:58:36 +0000

    wrote/passed test for showing original hp of player2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,4 +6,14 @@ feature 'Two players enter names' do
     click_button 'Submit'
     expect(page).to have_content 'James vs Jessica'
   end
+
+  scenario 'player1 can view player2\'s HP' do
+    visit('/')
+    fill_in :player1, with: 'James'
+    fill_in :player2, with: 'Jessica'
+    click_button 'Submit'
+    expect(page).to have_content 'Jessica: 100hp'
+  end
+
+
 end
