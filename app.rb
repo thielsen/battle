59396f540f8a25eb6dab1cece900a2e55b477fdf
James <james.rudrum@hyperisland.se>
2019-01-24 14:54:43 +0000

    enabled session, new route that renders play erb, get name route to redirect to play route

--- app.rb
+++ app.rb
@@ -2,13 +2,25 @@ require 'sinatra/base'
 require 'shotgun'
 
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+  #  @player1 = params[:player1]
+  #  @player2 = params[:player2]
+  #  erb(:play)
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
 
